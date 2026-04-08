d6bfdb6ca2bf76fbc06bdb5bffb921a74b7157f0
RobertLehmann <robert.lehmann0808@gmx.de>
2026-04-08 11:56:45 +0200

    UI Design Geräte

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,44 +1,55 @@
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-})
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div class="col-12 col-md-6">
+    <h3 class="display-6">Hallo, Test!</h3> <br/>
+    <div class="card">
+      <div class="card-header">
+        Geräte
+      </div>
+
+      <ul class="list-group list-group-flush">
+
+        <li class="list-group-item">
+
+          <div class="d-flex justify-content-between align-items-start w-100">
+
+            <div class="flex-grow-1 border rounded p-2 me-3">
+
+              <div class="d-flex align-items-center gap-3">
+                <strong>Testname</strong>
+                <small class="text-muted">ID: 12345</small>
+              </div>
+
+              <div class="mt-2 d-flex align-items-center gap-2">
+                <small>Koordinaten: 1 1</small>
+              </div>
+
+            </div>
+
+            <div class="d-flex flex-column gap-2">
+              <button class="btn btn-sm btn-primary">
+                <i class="bi bi-pencil-square"></i>
+              </button>
+              <button class="btn btn-sm btn-danger">
+                <i class="bi bi-trash3-fill"></i>
+              </button>
+            </div>
+          </div>
+
+        </li>
+
+      </ul>
+
+      <div class="card-body">
+        <button class="btn btn-success w-100">+ Gerät hinzufügen</button>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
 
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
 </style>
